17389b348520cd3228f3e2024305c1566ec59eab
Arthur Lockman <hello@rthr.me>
2014-04-08 15:23:00 -0400

    Added some class files to fill with code.

---
+++ src/Date.java
@@ -0,0 +1,39 @@
+public class Date
+{
+	private int month, day, year;
+
+	Date()
+	{
+
+	}
+
+	public void setDay(int day)
+	{
+		this.day = day;
+	}
+
+	public void setMonth(int month)
+	{
+		this.month = month;
+	}
+
+	public void setYear(int year)
+	{
+		this.year = year;
+	}
+
+	public int getDay()
+	{
+		return day;
+	}
+
+	public int getMonth()
+	{
+		return month;
+	}
+
+	public int getYear()
+	{
+		return year;
+	}
+}
---
+++ src/WeatherMonitor.java
@@ -0,0 +1,12 @@
+public class WeatherMonitor
+{
+	public int averageHighForMonth(int month)
+	{
+		return 1;
+	}
+
+	public int averageLowForMonth(int month)
+	{
+		return 1;
+	}
+}
